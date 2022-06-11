f0e920ee27cfd0849c8b4a0ca9df452ca03b1e4b
Gulam Anas <g.anas786@gmail.com>
2022-06-11 10:40:52 +0530

    updating directory page with directory component

---
+++ src/component/directory/Directory.js
@@ -0,0 +1,17 @@
+import Category from "../category/Category"
+import './Directory.style.scss'
+
+const Directory = ({ categories }) => {
+    return (
+        <div className="directory-container">
+            {categories.map((category) => {
+                return (
+                    <Category key={category.id} category={category} />
+                )
+            })}
+
+        </div>
+    )
+}
+
+export default Directory;
